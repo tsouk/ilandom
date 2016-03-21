accdf821eff4e4703426cbcef1f2c06157960ca3
Nikolas Tsouk <tsouk.n@gmail.com>
2016-03-21 15:53:21 +0000

    Adding url input page and url validation

--- routes/html2graph.js
+++ routes/html2graph.js
@@ -1,11 +1,18 @@
 var express = require('express');
 var router = express.Router();
 var jsdom = require('jsdom');
+var validator = require('validator');
 var recurseDomChildren = require('../public/javascripts/recurseDomChildren');
 var dom2json = require('../public/javascripts/dom2json');
 
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+    res.render('html2graphinput');
+});
+
 /* Render HTML 2 GRAPH */
-router.get('/', function(req, res) {
+router.post('/', function(req, res) {
     var jsdomConfig = {
         agentOptions: { 
             keepAlive: true,
@@ -15,28 +22,50 @@ router.get('/', function(req, res) {
             maxSockets: 1
         }
     };
-    jsdom.env(req.query.url,
-      function(err, window) {
-            if (!err) {
-                //call gethtmlnode here?
 
-                var windowJSON = dom2json.toJSON(window.document);
+    var url = req.param('url');
+    
+    // Sanitise and add http if it does not exist
+    if ( validator.isURL(url) ) {
+        url = addhttp(url);
+    }
+
+    if (url && url != '') {
+        jsdom.env(url,
+          function(err, window) {
+                if (!err) {
+                    //call gethtmlnode here?
+
+                    var windowJSON = dom2json.toJSON(window.document);
 
 
-                res.render('html2graph', { 
-                    title: 'Html to Graph',
-                    hud: 'The Force Directed Graph for ' + req.query.url,
-                    data: !windowJSON ? '' : JSON.stringify(windowJSON)
-                });
+                    res.render('html2graph', { 
+                        title: 'Html to Graph',
+                        hud: 'The Force Directed Graph for ' + url,
+                        data: !windowJSON ? '' : JSON.stringify(windowJSON)
+                    });
 
-                window.close();
+                    window.close();
+                }
+                else {
+                    console.log('[jsdom] ' + err + ', retrieving dom for [' + url + ']');
+                }
+
             }
-            else {
-                console.log('[jsdom] ' + err + ', retrieving dom for [' + req.query.url + ']');
-            }
+        );
+    }
+    else {
+        res.render('html2graphempty');
+    }
 
-        }
-    );
+    
 });
+
+function addhttp(url) {
+    if (!/^https?\:\/\//.test(url)) {
+        url = "http://" + url;
+    }
+    return url;
+}
 
 module.exports = router;
